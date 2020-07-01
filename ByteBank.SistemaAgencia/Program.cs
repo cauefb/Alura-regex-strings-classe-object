eed51fc3d322dffa1fd346e80293a89e696be395
cauê ferreira baraúna <kau3hc@hotmail.com>
2020-07-01 11:00:33 -0300

    aula 2

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+
+            string palavra = "moedaOrigen=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            
+            Console.WriteLine(indice);
+            Console.WriteLine("Tamanho da string nomeArgumento : " + nomeArgumento.Length);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.ReadLine();
+
+
+            //testando isNullOrEmpty
+            string textoVazio = "";
+            string textoNulo = null;
+
+            Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+
+            string url = "";
+
+            int indiceInterrogacao = url.LastIndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+
+         
+      
+
+            
+        }
+    }
+
+
+}
